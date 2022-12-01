5b85a06b1c02d25a474c57b0d531f4998ebbcc63
mkochmanski18 <104156994+mkochmanski18@users.noreply.github.com>
2022-12-01 17:16:35 +0100

    Admin features addition

--- sess/src/admin/admin.controller.ts
+++ sess/src/admin/admin.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, HttpException } from "@nestjs/common";
+import { Body, Controller, Get, HttpException, Param } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { userData } from "src/utils/interfaces";
+import internal from "stream";
 import { AdminService } from "./admin.service";
 
 @ApiTags('admin')
@@ -11,33 +12,40 @@ export class AdminController {
   //Users
   @Get('/users/all')
   @ApiOperation({ summary: 'User registration' })
-  @ApiResponse({ status: 200, description: 'User registered successfuly.'})
-  @ApiResponse({ status: 406, description: 'User already exists'})
-  @ApiResponse({ status: 409, description: 'User name is taken'})
+  @ApiResponse({ status: 200, description: 'Users found.'})
   @ApiResponse({ status: 500, description: 'Internal server error'})
   getUsers(){
       return this.adminService.getUsers();
   }
 
   //Named Conversations 
-  @Get('/namedConversations/all')
+  @Get('/conversation/allnamed')
   @ApiOperation({ summary: 'User registration' })
-  @ApiResponse({ status: 200, description: 'User registered successfuly.'})
-  @ApiResponse({ status: 406, description: 'User already exists'})
-  @ApiResponse({ status: 409, description: 'User name is taken'})
+  @ApiResponse({ status: 200, description: 'Conversations found.'})
   @ApiResponse({ status: 500, description: 'Internal server error'})
   getNamedConversations(){
       return this.adminService.getNamedConversations();
   }
 
   //Messages
-  @Get('/messages/count')
+  @Get('/conversation/count/:range')
   @ApiOperation({ summary: '' })
-  @ApiResponse({ status: 200, description: 'User registered successfuly.'})
-  @ApiResponse({ status: 406, description: 'User already exists'})
-  @ApiResponse({ status: 409, description: 'User name is taken'})
+  @ApiResponse({ status: 200, description: 'Conversations found.'})
   @ApiResponse({ status: 500, description: 'Internal server error'})
-  getMessagesByDate(){
-      return this.adminService.getMessages();
+  getConversationsCounterByDate(
+    @Param('range') range:string,
+  ){
+      return this.adminService.getConversationsCounter(range);
+  }
+
+  //Messages
+  @Get('/message/count/:range')
+  @ApiOperation({ summary: '' })
+  @ApiResponse({ status: 200, description: 'Messages found.'})
+  @ApiResponse({ status: 500, description: 'Internal server error'})
+  getMessagesCounterByDate(
+    @Param('range') range:string
+  ){
+      return this.adminService.getMessagesCounter(range);
   }
 }
--- sess/src/admin/admin.service.ts
+++ sess/src/admin/admin.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { Conversation } from "src/conversation/conversation.entity";
 import { Message } from "src/message/message.entity";
+import { User } from "src/user/user.entity";
 import { UserService } from "src/user/user.service";
 import { MessagesQueryTypeEnum } from "src/utils/types";
 import { getManager, getRepository } from "typeorm";
@@ -11,17 +12,34 @@ export class AdminService {
     constructor(private readonly userService: UserService) {}
 
     getUsers(){
-        const users = this.userService.getUsers();
+        const users = User.query('select userid,name,firstname,lastname,sex,email,confirmed,role,photo from user group by role,confirmed');
         return users;
     }
 
+    async getRegistrationsCounter(range:string){
+        if(range === '0') return await User.query('select SUBSTRING(createdAt,1,10) as okres,count(userd) as liczba from user group by createdAt');
+        else if(range === '1' ) return await User.query('select SUBSTRING(createdAt,1,7) as okres,count(userd) as liczba from user group by okres');
+        else if(range === '2') return await User.query('select SUBSTRING(createdAt,1,4) as okres,count(userd) as liczba from user group by okres');
+        else return [];
+    }
+
     getNamedConversations() {
-        const conversations = Conversation.query('select * from conversation where name is not $1',['Regular Conversation']);
+        const conversations = Conversation.query('select * from conversation group by conversationType');
         return conversations;
       }
 
-    async getMessages(){
-        const res = await Message.query('select date,count(messageId) from message group by date');
-        return res;
+    async getConversationsCounter(range:string){
+        if(range === '0') return await Conversation.query('select SUBSTRING(createdAt,1,10) as okres,count(conversationId) as liczba from conversation group by createdAt');
+        else if(range === '1' ) return await Conversation.query('select SUBSTRING(createdAt,1,7) as okres,count(conversationId) as liczba from conversation group by okres');
+        else if(range === '2') return await Conversation.query('select SUBSTRING(createdAt,1,4) as okres,count(conversationId) as liczba from conversation group by okres');
+        else return [];
+    }
+
+    async getMessagesCounter(range:string){
+        //return await Message.query('select SUBSTRING(date,1,5) as "okres" from message group by date');
+        if(range === '0') return await Message.query('select date as okres,count(messageId) as liczba from message group by date order by okres');
+        if(range === '1') return await Message.query('select SUBSTRING(date,4,8) as okres,count(messageId) as liczba from message group by okres order by okres');
+        if(range === '2') return await Message.query('select SUBSTRING(date,7,4) as okres,count(messageId) as liczba from message group by okres order by okres');
+        else return [];
     }
     }
